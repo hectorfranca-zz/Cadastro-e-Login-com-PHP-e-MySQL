264c04c822d6fff51f81cd6d5bdade4a8ffffbd3
Héctor França <hectorjuliomartinsdefranca@hotmail.com>
2020-05-26 22:59:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function Login() {
+    window.location="login.php";
+    }
+
+function Cadastro() {
+    window.location="cadastro.php";
+    }
+
+function Sair() {
+    window.location="logout.php";
+    }
