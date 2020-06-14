80507bd592388aab43e37e9c9424b730557dcd1f
Héctor França <hectorjuliomartinsdefranca@hotmail.com>
2020-06-14 20:12:41 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define ('hostname', 'sql305.epizy.com');
-define ('user', 'epiz_25824494');
-define ('password', '64B73ZqXqC7');
-define ('database', 'epiz_25824494_cadastro');
-$conexao = mysqli_connect(hostname, user, password, database) or die ('Não foi possível conectar ao banco de dados');
-?>
